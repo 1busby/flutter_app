5201b3266de6b26f5526cf1f690ee91179adb314
Michael Busby <michaelb@clearlyinnovative.com>
2019-01-03 01:11:24 -0500

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Container(
+    MyStatelessWidget()
+  );
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       color: Colors.deepPurple,
       child: Center(
         child: Text("Hello from Flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 32.0, color: Colors.white))
       )
-    )
-  );
+    );
+  }
 }
